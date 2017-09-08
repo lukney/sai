98057d4dd978aa2997e1ba13e1f169f19ad5ee1e
lukney <sangeetha18lukney@gmail.com>
2017-09-08 04:34:55 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+
+//start mysql connection
+var connection = mysql.createConnection({
+  host     : 'sangeetha.cgcpmi6mcuyv.us-east-2.rds.amazonaws.com', //mysql database host name
+  user     : 'sangeetha', //mysql database user name
+  password : '12345678', //mysql database password
+  database : 'sangeetha', //mysql database name
+  port     : '3306'  
+});
+
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected with mysql database...')
+})
+//end mysql connection
+
+//start body-parser configuration
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+//end body-parser configuration
+
+//create app server
+var server = app.listen(3001,   function () {
+
+  var host = 'ec2-18-221-18-150.us-east-2.compute.amazonaws.com'
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+});
+
+//rest api to get all customers
+app.get('/customer', function (req, res) {
+   connection.query('select * from karnataka', function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
